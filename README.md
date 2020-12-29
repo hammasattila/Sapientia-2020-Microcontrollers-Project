115372ec47eab525e01c0490fb6d9dd00474bb7d
Attila Hammas <hammasattila@outlook.com>
2020-12-29 15:38:28 +0200

    Basic init. Serial dump RFID data.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Sapientia-Microcontrollers2020-Project
 
 Durin Microcontrollers Cuorse my project was an RFID Autorization System.
+
+## Configration
+
+Create an *EnviromentVariables.h* header file in your include directory. This should contain the folowing enviroment definitions:
+
+```c++
+#define WIFI_SSID "<wifi ssid name here>"
+#define WIFI_PSWD "<wifi pasword here>"
+
+const byte P_CHIP_SELECT_SPI = GPIO_NUM_15;
+const byte P_RFID_RESET = GPIO_NUM_27;
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,64 @@
+#include "EnviromentVariables.h"
+
 #include <Arduino.h>
+#include <MFRC522.h>
+#include <WiFi.h>
+
+#define PIN_CHIP_SELECT_SPI GPIO_NUM_15
+#define PIN_RFID_RESET GPIO_NUM_27
+
+/// GLOBAL VARIABLES
+MFRC522 mfrc522(PIN_CHIP_SELECT_SPI, PIN_RFID_RESET);
+MFRC522::MIFARE_Key key;
 
 void setup() {
-  // put your setup code here, to run once:
+
+    delay(1000);
+
+    /// INIT SERIAL COMUNICATION.
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(100);
+    }
+
+    /// CONNECTING TIO THE NETWORK.
+    WiFi.begin(WIFI_SSID, WIFI_PSWD);
+    Serial.print(F("Connecting to WiFi"));
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(250);
+        Serial.print(F("."));
+    }
+    Serial.println();
+    Serial.println(F("Connected to the WiFi succesfully."));
+    Serial.print(F("Device's IP addres is: "));
+    Serial.println(WiFi.localIP());
+
+    /// INITIALIZING THE RFID READER
+    Serial.println(F("Initializing SPI bus."));
+    SPI.begin(GPIO_NUM_14, GPIO_NUM_12, GPIO_NUM_13, GPIO_NUM_15);
+    delay(1000);
+    Serial.println(F("Initializing MFRC522 reader."));
+    mfrc522.PCD_Init();
+    Serial.print(F("MFRC522 Digital self test: "));
+    Serial.println(mfrc522.PCD_PerformSelfTest() ? F("OK") : F("DEFECT or UNKNOWN"));
+    mfrc522.PCD_DumpVersionToSerial();
+    mfrc522.PCD_Init();
+
+    Serial.println();
+    Serial.println(F("#### SYSTEM READY TO GO ####"));
+    Serial.println();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+        // sleep(500);
+        return;
+    }
+
+    if (!mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+
+    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
